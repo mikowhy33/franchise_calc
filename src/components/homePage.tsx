274d43a9e0060af63a5f047e8f051f9ef93bd53f
mikowhy33 <koding252@gmail.com>
2025-09-23 11:59:38 +0200

    [feat]- changes, gotta fix the months hooks

--- src/components/homePage.tsx
+++ src/components/homePage.tsx
@@ -27,32 +27,34 @@ import { HtmlContext } from "next/dist/server/route-modules/pages/vendored/conte
 export default function MainnnPage() {
 	const { setTheme } = useTheme();
 
-	const nrofStudsref = React.useRef<any>(0);
-	const [nrOfStudents, setnrOfStudents] = useState(0);
+	//Monthly Info
 
-	const [nrOfMeetings, setnrOfMeetings] = useState(0);
+	const [nrOfStudPIE, setNrOfStudsPIE] = useState(0);
+	const [nrOfStudPEL, setNrOfStudsPEL] = useState(0);
+	const [nrOfTeachers, setNrOfTeachers] = useState(0);
+	const [nrOfMeetingsPIE, setnrOfMeetingsPIE] = useState(0);
+	const [nrOfMeetingsPEL, setnrOfMeetingsPEL] = useState(0);
+	const [ratePerStudPerMeetingPIE, setratePerStudPerMeetingPIE] = useState(0);
+	const [ratePerStudPerMeetingPEL, setratePerStudPerMeetingPEL] = useState(0);
 
-	const [ratePerStudent, setRatePerStudent] = useState(0);
+	// Monthly Costs
 
-	const [togglingValue, settogglingValue] = useState(false);
-
-	let monthlyRevenue: number = 0;
-
-	const handlenrOfMeeting = (e: any) => {
-		setnrOfMeetings(e);
-	};
-
-	const handleRatePerStudent = (e: any) => {
-		setRatePerStudent(e);
-	};
+	const [studLicenses, setStudLicenses] = useState(0);
+	const [teacherCostPIE, setTeacherCostPIE] = useState(0);
+	const [teacherCostPEL, setTeacherCostPEL] = useState(0);
+	const [transportCostPIE, setTransportCostPIE] = useState(0);
+	const [transportCostPEL, setTransportCostPEL] = useState(0);
+	const [childSafetyCert, setChildSafetyCert] = useState(0);
 
 	const months = ["September", "October", "November"];
 
-	const revenue =
-		Number(nrofStudsref.current?.value) * nrOfMeetings * ratePerStudent;
-
 	const [avgsalaryassumption, setavgsalaryassumption] = useState(0);
 	const [workingdays, setworkingdays] = useState(0);
+
+
+    // outputs
+
+    const monthlyRevenuePIE=nrOfStudPIE*nrOfMeetingsPIE*ratePerStudPerMeetingPIE
 
 	const TeacherCostPerDay: number = Number(
 		(avgsalaryassumption / workingdays).toFixed(1)
@@ -116,6 +118,8 @@ export default function MainnnPage() {
 
 					{/* Middle column */}
 					<Card className="flex flex-col  p-10 w-full max-w-md">
+						<CardHeader>Monthly info</CardHeader>
+
 						<CardContent>
 							<div className=" space-y-3 ">
 								{months.map((month, index) => (
@@ -124,31 +128,182 @@ export default function MainnnPage() {
 
 										<div className="grid grid-cols-2 gap-2 text-xs border border-amber-50 p-3 rounded-2xl items-center">
 											<p>Nr of Students PIE</p>
-											<Input type="number"></Input>
+											<Input
+												type="number"
+												onChange={(e) => {
+													setNrOfStudsPIE(Number(e.target.value));
+												}}></Input>
 
 											<p>Nr of Students PEL</p>
-											<Input type="number"></Input>
+											<Input
+												type="number"
+												onChange={(e) => {
+													setNrOfStudsPEL(Number(e.target.value));
+												}}></Input>
 
 											<p>Nr of Teachers</p>
-											<Input type="number"></Input>
+											<Input
+												type="number"
+												onChange={(e) => {
+													setNrOfTeachers(Number(e.target.value));
+												}}></Input>
 
 											<p>Nr of Meetings PIE</p>
-											<Input type="number"></Input>
+											<Input
+												type="number"
+												onChange={(e) => {
+													setnrOfMeetingsPIE(Number(e.target.value));
+												}}></Input>
 
 											<p>Nr of Meeting PEL</p>
-											<Input type="number"></Input>
+											<Input
+												type="number"
+												onChange={(e) => {
+													setnrOfMeetingsPEL(Number(e.target.value));
+												}}></Input>
 
 											<p>Rate per Student per Meeting PIE</p>
-											<Input type="number"></Input>
+											<Input
+												type="number"
+												onChange={(e) => {
+													setratePerStudPerMeetingPIE(Number(e.target.value));
+												}}></Input>
 
 											<p>Rate per Student per Meeting PEL</p>
-											<Input type="number"></Input>
+											<Input
+												type="number"
+												onChange={(e) => {
+													setratePerStudPerMeetingPEL(Number(e.target.value));
+												}}></Input>
 										</div>
 									</div>
 								))}
 							</div>
+						</CardContent>
+					</Card>
 
-							<div className="p-2 space-y-4">
+					<Card>
+						<CardHeader>Costs</CardHeader>
+
+						<CardContent>
+							<div className=" space-y-3 ">
+								{months.map((month, index) => (
+									<div key={month}>
+										<h2>Month: {month}</h2>
+
+										<div className="grid grid-cols-2 gap-2 p-3 text-xs items-center border border-amber-50 rounded-2xl ">
+											<p>Student Licenses</p>
+											<Input
+												type="number"
+												onChange={(e) => {
+													setStudLicenses(Number(e.target.value));
+												}}></Input>
+
+											<p>Teacher Cost PIE</p>
+											<Input
+												type="number"
+												onChange={(e) => {
+													setTeacherCostPIE(Number(e.target.value));
+												}}></Input>
+
+											<p>Teacher Cost PEL</p>
+											<Input
+												type="number"
+												onChange={(e) => {
+													setTeacherCostPEL(Number(e.target.value));
+												}}></Input>
+
+											<p>Transport Cost PIE</p>
+											<Input
+												type="number"
+												onChange={(e) => {
+													setTransportCostPIE(Number(e.target.value));
+												}}></Input>
+
+											<p>Transport Cost PEL</p>
+											<Input
+												type="number"
+												onChange={(e) => {
+													setTransportCostPEL(Number(e.target.value));
+												}}></Input>
+
+											<p>Child Safety Certifications</p>
+											<Input
+												type="number"
+												onChange={(e) => {
+													setChildSafetyCert(Number(e.target.value));
+												}}></Input>
+										</div>
+									</div>
+								))}
+							</div>
+						</CardContent>
+					</Card>
+
+					<Card>
+						<CardHeader>Calculations output</CardHeader>
+
+						<CardContent>
+							<div className="overflow-x-auto">
+								<table className="w-full text-sm">
+									<thead>
+										<tr>
+											<th className="text-left">Metric / Month</th>
+											{months.map((month) => (
+												<th key={month}>{month}</th>
+											))}
+										</tr>
+									</thead>
+
+
+									<tbody>
+										<tr>
+											<td className="p-2 font-medium text-left">Monthly Revenue PIE</td>
+
+                                            {months.map((motnh,index)=>(
+                                                <td key={index}>{monthlyRevenuePIE}</td>
+                                            ))}
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</CardContent>
+					</Card>
+				</div>
+			</div>
+		</main>
+	);
+}
+
+/*
+NO LONGER IN USE 4 TESTING USEREF AND USESTATE
+
+
+const nrofStudsref = React.useRef<any>(0);
+	
+
+	const [nrOfMeetings, setnrOfMeetings] = useState(0);
+
+	const [ratePerStudent, setRatePerStudent] = useState(0);
+
+	
+
+	let monthlyRevenue: number = 0;
+
+	const handlenrOfMeeting = (e: any) => {
+		setnrOfMeetings(e);
+	};
+
+	const handleRatePerStudent = (e: any) => {
+		setRatePerStudent(e);
+	};
+
+    
+	const revenue =
+		Number(nrofStudsref.current?.value) * nrOfMeetings * ratePerStudent;
+
+
+<div className="p-2 space-y-4">
 								<div className="space-y-1">
 									<p>Nr of students</p>
 									<Input type="number" ref={nrofStudsref}></Input>
@@ -183,9 +338,9 @@ export default function MainnnPage() {
 									</p>
 								</div>
 							</div>
-						</CardContent>
 
-						<Button
+
+                            <Button
 							onClick={() => {
 								settogglingValue((p) => !p);
 								if (nrofStudsref.current) {
@@ -194,9 +349,5 @@ export default function MainnnPage() {
 							}}>
 							Pokaż wartość
 						</Button>
-					</Card>
-				</div>
-			</div>
-		</main>
-	);
-}
+
+*/
